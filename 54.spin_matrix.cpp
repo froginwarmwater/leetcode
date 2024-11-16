0632bbbd9264e1f98991168fe9bde889bafb4304
froginwarmwater <goforwardlee@gmail.com>
2024-11-16 14:55:57 +1000

    question 54

---
+++ 54.spin_matrix.cpp
@@ -0,0 +1,35 @@
+#include "common.h"
+using namespace std;
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        // right, down, left, up
+        vector<vector<int>> direction = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        vector<vector<bool>> visited(row, vector<bool>(col, false));
+        int currentRow = 0;
+        int currentCol = 0;
+        int length = row * col;
+        int directionIndex = 0;
+        vector<int> ans;
+        for(int i = 0; i < length; i ++) {
+            ans.push_back(matrix[currentRow][currentCol]);
+            visited[currentRow][currentCol] = true;
+            currentRow += direction[directionIndex][0];
+            currentCol += direction[directionIndex][1];
+            int nextRow = currentRow + direction[directionIndex][0];
+            int nextCol = currentCol + direction[directionIndex][1];
+            if(visited[nextRow][nextCol] || nextRow < 0 || nextRow == row || nextCol < 0 || nextCol == col)
+                directionIndex = (directionIndex + 1) % 4;
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<vector<int>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    solution.spiralOrder(matrix);
+}
---
+++ common.h
@@ -0,0 +1,14 @@
+// common.h
+#pragma once  // 防止重复包含
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <list>
+#include <array>
+#include <unordered_map>
